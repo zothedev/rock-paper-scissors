e0650c511fc3de23eccb1cc456bdd12e199783c6
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-04 21:45:30 -0500

    Add logic to get computers answer choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+function getComputerChoice() {
+    // make computer choice (0-2)
+    let number = randomIntFromZeroAndTwo();
+
+    // return "rock" if number is 0
+    if (number === 0) {
+        return "rock";
+    } // return "paper" if number is 1
+    else if (number === 1) {
+        return "paper";
+    } // otherwise, return "scissors"
+    else {
+        return "scissors";
+    }
+}
+
+// this function returns integers between 0-2 (inclusive)
+let randomIntFromZeroAndTwo = () => Math.floor(Math.random() * 3)
+
+console.log(getComputerChoice());
