54c3ccf3befeff234f174f72972c8d16f978c3ad
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-04 22:25:13 -0500

    Add logic to get user answer choice

--- script.js
+++ script.js
@@ -17,4 +17,41 @@ function getComputerChoice() {
 // this function returns integers between 0-2 (inclusive)
 let randomIntFromZeroAndTwo = () => Math.floor(Math.random() * 3)
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function getUserChoice() {
+
+    // run this loop until user enters a valid answer
+    while (true) {
+        // ask the user to choose a number 0-2, corresponding to rock, paper, or scissors. convert this value to number.
+        let userChoice = +(prompt("Please enter: \n0 for Rock, \n1 for Paper, \nor 2 for Scissors!", 0));
+    
+        // verify that userChoice is valid using a helper function
+        if (isValidAnswer(userChoice)) {
+            // return rock, paper, or scissors based on user input
+            if (userChoice === 0) {
+                return "rock";
+            } else if (userChoice === 1) {
+                return "paper";
+            } else {
+                return "scissors";
+            }
+        }
+        // if the user didn't input a valid answer, alert them and return to start of loop
+        alert("Make sure you input either 0, 1, or 2!")
+    }
+    
+
+}
+
+function isValidAnswer(userChoice) {
+    if (userChoice == NaN) {
+        return false;
+    }
+    if (userChoice > 2 || userChoice < 0) {
+        return false;
+    }
+    return true;
+}
+
+console.log(getUserChoice());
