3c523adf2e7ad339b09248a65643ec0019122581
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-14 18:34:26 -0500

    Remove getUserChoice() and other unworking code

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-let results = document.querySelector("div")
+let results = document.querySelector(".results");
+let selections = document.querySelector(".selections");
+let userScorePara = document.querySelector("#userScore");
+let scores = document.querySelector(".scores");
+let computerScorePara = document.querySelector("#computerScore");
+let userChoicePara = document.createElement("p");
+let computerChoicePara = document.createElement("p");
+let roundResultPara = document.createElement("p");
 
-let rockButton = document.querySelector('#rock');
-let paperButton = document.querySelector('#paper');
-let scissorsButton = document.querySelector('#scissors');
+selections.style.display = "none";
+scores.style.display = "none";
 
-rockButton.addEventListener("click", () => {
-    playRound("rock");
-});
-paperButton.addEventListener("click", () => {
-    playRound("paper");
-});
-scissorsButton.addEventListener("click", () => {
-    playRound("scissors");
-});
 
+// if user selects button x, playRound(x) is called with x as the user selection
+selections.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case "rock":
+            playRound("rock");
+            break;
+        case "paper":
+            playRound("paper");
+            break;
+        case "scissors":
+            playRound("scissors");
+            break;
+    }
+});
 
 function getComputerChoice() {
     // make computer choice (0 to 2)
@@ -34,29 +47,6 @@ function getComputerChoice() {
 // this function returns integers between 0 to 2 (inclusive)
 function randomIntFromZeroAndTwo() {
     return Math.floor(Math.random() * 3);
-} 
-
-function getUserChoice() {
-
-    // run this loop until user enters a valid answer
-    while (true) {
-        // ask the user to choose a number 0 to 2, corresponding to rock, paper, or scissors. convert this value to number.
-        let userChoice = +(prompt("Please enter: \n0 for Rock, \n1 for Paper, \nor 2 for Scissors!", 0));
-
-        // verify that userChoice is valid using a helper function
-        if (isValidAnswer(userChoice)) {
-            // return rock, paper, or scissors based on user input
-            if (userChoice === 0) {
-                return "rock";
-            } else if (userChoice === 1) {
-                return "paper";
-            } else {
-                return "scissors";
-            }
-        }
-        // if the user didn't input a valid answer, alert them and return to start of loop
-        alert("Make sure you input either 0, 1, or 2!")
-    }
 }
 
 // this function returns true only if the userChoice is valid (0 to 2)
@@ -76,11 +66,11 @@ function isValidAnswer(userChoice) {
 // this function plays 1 round of rock paper scissors
 function playRound(userChoice) {
 
-    const computerChoice = getComputerChoice();
+    userChoicePara.textContent = "";
+    computerChoicePara.textContent = "";
+    roundResultPara.textContent = "";
 
-    let userChoicePara = document.createElement("p");
-    let computerChoicePara = document.createElement("p");
-    let roundResultPara = document.createElement("p");
+    const computerChoice = getComputerChoice();
 
     userChoicePara.textContent = `User Chooses: ${userChoice}`;
     computerChoicePara.textContent = `Computer Chooses: ${computerChoice}`;
@@ -133,60 +123,32 @@ function playRound(userChoice) {
     }
 
     // user win numbers
-    if (choiceLengthDiff === -4 || 
-        choiceLengthDiff === 1 || 
-        choiceLengthDiff === 3 ) {
-            didUserWin = true;
-        } else {
-            didUserWin = false;
-        }
-        return didUserWin;
-
-    
-
+    if (choiceLengthDiff === -4 ||
+        choiceLengthDiff === 1 ||
+        choiceLengthDiff === 3) {
+        didUserWin = true;
+    } else {
+        didUserWin = false;
+    }
+    return didUserWin;
 }
 
 // this function controls the entire multi-round game 
-// function playGame() {
+function playGame() {
     // initialize the score values to 0
-    // let userScore = 0;
-    // let computerScore = 0;
+    let userScore = 0;
+    let computerScore = 0;
 
     // set starting round and declare result
-    // let round = 1;
-    // let didUserWin;
+    let round = 1;
+    let didUserWin;
 
-    // we want the game to run for 5 rounds.
-    // while (round <= 5) {
-        // console.log(`----- START OF ROUND ${round} -----`)
+    let startGameBtn = document.querySelector("button");
 
-        // didUserWin = playRound();
-
-        // if (didUserWin) {
-        //     userScore++;
-        // } else if (didUserWin === false) {
-        //     computerScore++
-        // }
-
-        // console.log(`----- END OF ROUND ${round} -----`)
-        // console.log("User Score: " + userScore);
-        // console.log("Computer Score: " + computerScore);
-
-        // round++;
-    // }
-
-    // ~~end of game prints~~
-
-    // console.log("----- MATCH OVER! -----")
-    // if (userScore == computerScore) {
-    //     console.log("Tie match! Both you and the computer scored: " + userScore);
-    // } else if (userScore > computerScore) {
-    //     console.log("You win! You scored " + userScore + " points and the computer scored " + computerScore + " points!")
-    // } else {
-    //     console.log("You lose! You scored " + userScore + " points and the computer scored " + computerScore + " points!")
-    // }
-// }
-
-// playGame();
-
-// playRound();
+    startGameBtn.addEventListener("click", e => {
+        startGameBtn.style.display = "none";
+        selections.style.display = "block";
+        scores.style.display = "block";
+    });
+}
+playGame();
